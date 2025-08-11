fd9f13cea8b307a2936c5103bfc978836ff37a13
João Alóca <125145954+joao-aloca@users.noreply.github.com>
2025-08-11 17:41:22 -0300

    Declaração de variáveis para facilitação de futuras alterações

--- coleta-informacoes-computador-windows-wmic.bat
+++ coleta-informacoes-computador-windows-wmic.bat
@@ -2,62 +2,65 @@ REM CRIADOR: João Alóca
 REM DATA DE CRIAÇÃO: 11/08/2025
 REM OBJETIVO: coletar informações das máquinas para atualização de documentação técnica
 
-mkdir %username%-temp
-cd %username%-temp
-mkdir csv
+set HOME = %homepath%
+set MAIN_DIR=%username%-computador-specs
+set CSV_DIR=csv
+set TXT_FILE=%username%_geral.txt
 
-hostname > %username%_geral.txt
-echo "Home: %homepath%" >> %username%_geral.txt
+mkdir %MAIN_DIR%
+mkdir %MAIN_DIR%\%CSV_DIR%
 
-echo ---------------------------------------------------------- >> %username%_geral.txt
-echo PATH >> %username%_geral.txt
-wmic /APPEND:%username%_geral.txt path softwarelicensingservice get OA3xOriginalProductKey /FORMAT:list
-wmic /OUTPUT:csv\%username%_windows_license_key.csv path softwarelicensingservice get OA3xOriginalProductKey /FORMAT:csv
+cd %MAIN_DIR%
 
-echo ---------------------------------------------------------- >> %username%_geral.txt
-echo CPU >> %username%_geral.txt
-wmic /APPEND:%username%_geral.txt cpu GET /ALL /FORMAT:list
-wmic /OUTPUT:csv\%username%_cpu.csv cpu GET /ALL /FORMAT:csv
+hostname > %TXT_FILE%
+echo %username% >> %TXT_FILE%
 
-echo ---------------------------------------------------------- >> %username%_geral.txt
-echo DISKDRIVE >> %username%_geral.txt
-wmic /APPEND:%username%_geral.txt diskdrive GET /ALL /FORMAT:list
-wmic /OUTPUT:csv\%username%_diskdrive.csv diskdrive GET /ALL /FORMAT:csv
+echo ---------------------------------------------------------- >> %TXT_FILE%
+echo COMPUTERSYSTEM >> %TXT_FILE%
+wmic /APPEND:%TXT_FILE% computersystem GET /ALL /FORMAT:list
+wmic /OUTPUT:%CSV_DIR%\%username%_computersystem.csv computersystem GET /ALL /FORMAT:csv
 
-echo ---------------------------------------------------------- >> %username%_geral.txt
-echo MEMORYCHIP >> %username%_geral.txt
-wmic /APPEND:%username%_geral.txt memorychip GET /ALL /FORMAT:list
-wmic /OUTPUT:csv\%username%_memorychip.csv memorychip GET /ALL /FORMAT:csv
+echo ---------------------------------------------------------- >> %TXT_FILE%
+echo CPU >> %TXT_FILE%
+wmic /APPEND:%TXT_FILE% cpu GET /ALL /FORMAT:list
+wmic /OUTPUT:%CSV_DIR%\%username%_cpu.csv cpu GET /ALL /FORMAT:csv
 
-echo ---------------------------------------------------------- >> %username%_geral.txt
-echo MEMPHYSICAL >> %username%_geral.txt
-wmic /APPEND:%username%_geral.txt memphysical GET /ALL /FORMAT:list
-wmic /OUTPUT:csv\%username%_memphysical.csv memphysical GET /ALL /FORMAT:csv
+echo ---------------------------------------------------------- >> %TXT_FILE%
+echo DISKDRIVE >> %TXT_FILE%
+wmic /APPEND:%TXT_FILE% diskdrive GET /ALL /FORMAT:list
+wmic /OUTPUT:%CSV_DIR%\%username%_diskdrive.csv diskdrive GET /ALL /FORMAT:csv
 
-echo ---------------------------------------------------------- >> %username%_geral.txt
-echo COMPUTERSYSTEM >> %username%_geral.txt
-wmic /APPEND:%username%_geral.txt computersystem GET /ALL /FORMAT:list
-wmic /OUTPUT:csv\%username%_computersystem.csv computersystem GET /ALL /FORMAT:csv
+echo ---------------------------------------------------------- >> %TXT_FILE%
+echo MEMORYCHIP >> %TXT_FILE%
+wmic /APPEND:%TXT_FILE% memorychip GET /ALL /FORMAT:list
+wmic /OUTPUT:%CSV_DIR%\%username%_memorychip.csv memorychip GET /ALL /FORMAT:csv
 
-echo ---------------------------------------------------------- >> %username%_geral.txt
-echo OS >> %username%_geral.txt
-wmic /APPEND:%username%_geral.txt os GET /ALL /FORMAT:list
-wmic /OUTPUT:csv\%username%_os.csv os GET /ALL /FORMAT:csv
+echo ---------------------------------------------------------- >> %TXT_FILE%
+echo MEMPHYSICAL >> %TXT_FILE%
+wmic /APPEND:%TXT_FILE% memphysical GET /ALL /FORMAT:list
+wmic /OUTPUT:%CSV_DIR%\%username%_memphysical.csv memphysical GET /ALL /FORMAT:csv
 
-echo ---------------------------------------------------------- >> %username%_geral.txt
-echo NIC >> %username%_geral.txt
-wmic /APPEND:%username%_geral.txt nic GET /ALL /FORMAT:list
-wmic /OUTPUT:csv\%username%_nic.csv nic GET /ALL /FORMAT:csv
+echo ---------------------------------------------------------- >> %TXT_FILE%
+echo OS >> %TXT_FILE%
+wmic /APPEND:%TXT_FILE% os GET /ALL /FORMAT:list
+wmic /OUTPUT:%CSV_DIR%\%username%_os.csv os GET /ALL /FORMAT:csv
 
-echo ---------------------------------------------------------- >> %username%_geral.txt
-echo NICCONFIG >> %username%_geral.txt
-wmic /APPEND:%username%_geral.txt nicconfig GET /ALL /FORMAT:list
-wmic /OUTPUT:csv\%username%_nicconfig.csv nicconfig GET /ALL /FORMAT:csv
+echo ---------------------------------------------------------- >> %TXT_FILE%
+echo NIC >> %TXT_FILE%
+wmic /APPEND:%TXT_FILE% nic GET /ALL /FORMAT:list
+wmic /OUTPUT:%CSV_DIR%\%username%_nic.csv nic GET /ALL /FORMAT:csv
 
-echo ---------------------------------------------------------- >> %username%_geral.txt
-echo BIOS >> %username%_geral.txt
-wmic /APPEND:%username%_geral.txt bios GET /ALL /FORMAT:list
-wmic /OUTPUT:csv\%username%_bios.csv bios GET /ALL /FORMAT:csv
+echo ---------------------------------------------------------- >> %TXT_FILE%
+echo BIOS >> %TXT_FILE%
+wmic /APPEND:%TXT_FILE% bios GET /ALL /FORMAT:list
+wmic /OUTPUT:%CSV_DIR%\%username%_bios.csv bios GET /ALL /FORMAT:csv
+
+
+REM  Coleta da Chave de Licença do Windows (BIOS)
+echo ---------------------------------------------------------- >> %TXT_FILE%
+echo PATH >> %TXT_FILE%
+wmic /APPEND:%TXT_FILE% path softwarelicensingservice get OA3xOriginalProductKey /FORMAT:list
+wmic /OUTPUT:%CSV_DIR%\%username%_windows_license_key.csv path softwarelicensingservice get OA3xOriginalProductKey /FORMAT:csv
 
 cd ..
-tar -czvf %username%-inventario-ti.tar.gz %username%-temp
+tar -czvf %MAIN_DIR%.tar.gz %MAIN_DIR%
